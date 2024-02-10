6c8c5fda646a38d9af6a455c7c7913597a24e88b
Arvind w <arvindw@sankalp.com>
2024-02-10 16:50:54 +0530

    add open app method

--- src/android/BroadcastRegister.java
+++ src/android/BroadcastRegister.java
@@ -1,11 +1,45 @@
 package cordova.plugin.broadcast.register;
 
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.CallbackContext;
+import cordova.plugin.broadcast.register.*;
 
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.app.Activity;
+import android.content.*;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.content.pm.LabeledIntent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.text.Html;
+import android.util.Base64;
+import android.view.Gravity;
+import android.widget.Toast;
+import android.util.Log;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import android.content.Context;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -19,7 +53,7 @@ public class BroadcastRegister extends CordovaPlugin {
         //     this.coolMethod(message, callbackContext);
         //     return true;
         // }
-        if(action.equals('add')) {
+        if(action.equals("add")) {
             this.add(args, callbackContext);
             return true;
         }
@@ -37,8 +71,8 @@ public class BroadcastRegister extends CordovaPlugin {
     private void add(JSONArray args, CallbackContext callbackContext) {
         if(args != null) {
             try {
-                let n1 = Integer.parseInt(args.getJSONObject(0).getString('param1'));
-                let n2 = Integer.parseInt(args.getJSONObject(0).getString('param2'));
+                int n1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int n2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
                 callbackContext.success(""+ (n1 + n2));
 
             }catch(Exception ex) {
@@ -46,7 +80,39 @@ public class BroadcastRegister extends CordovaPlugin {
             }
         }
         else {
-            callbackContext.error('parameters should not be undefined')''
+            callbackContext.error("parameters should not be undefined");
+        }
+    }
+
+        private void openApplication(JSONArray args, CallbackContext callbackContext) {
+        if(args != null) {
+            try {
+                final String packageName = args.getJSONObject(0).getString("package");
+                cordova.getThreadPool().execute(new SocialSharingRunnable(callbackContext) {
+                public void Run() {
+                    final Intent intent = new Intent(Intent.ACTION_VIEW);
+                    try {
+                        // intent.setPackage(packageName);
+                        intent.setData(Uri.parse("market://details?id=" + packageName));
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        cordova.getActivity().runOnUiThread(new Runnable() {
+                            public void run() {
+                            try {
+                                startActivity(intent);
+                            } catch (Exception e) {
+                                callbackContext.error(e.getMessage());
+                            }
+                            }
+                        });
+                    }catch(Exception ex) {
+                    callbackContext.error(e.getMessage());
+                    }
+                } 
+                })
+                return true;
+            }catch(Exception ex) {
+               callbackContext.error(e.getMessage()); 
+            }
         }
     }
 }
--- www/BroadcastRegister.js
+++ www/BroadcastRegister.js
@@ -2,5 +2,9 @@ var exec = require('cordova/exec');
 
 
 module.exports.add = function (arg0, success, error) {
-    exec(success, error, 'BroadcastRegister','add', [arg0]);
+    exec(success, error, "BroadcastRegister","add", [arg0]);
+}
+
+module.exports.openApplication = function (arg0, success, error) {
+    exec(success, error, "BroadcastRegister","openApplication", [arg0]);
 }
